fdc8b290696c9e129f22303683a74358d8a360b5
avih8r <sparco.209@gmail.com>
2017-09-15 15:43:44 +1000

    doing stuff

--- header.php
+++ header.php
@@ -19,7 +19,6 @@
 	<script src="js/bootstrap.min.js"></script>
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'dope_theme' ); ?></a>
